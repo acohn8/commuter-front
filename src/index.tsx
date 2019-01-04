7cac4f3cb5ec9165be18196027ee8f5eddffc3ba
Adam Cohn <adamcohn88@gmail.com>
2019-01-03 22:55:18 -0500

    added apollo

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import gql from 'graphql-tag';
 import { BrowserRouter as Router } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import App from './containers/App';
+
 require('dotenv').config();
 
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql'
+});
+
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
